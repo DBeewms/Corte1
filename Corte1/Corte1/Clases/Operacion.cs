8f245f35c03776c3910feeaf36f62b9d0eb29022
DBeewms <dbwcontrol05@gmail.com>
2024-09-23 08:34:30 -0600

    Agregada clase Operacion

---
+++ Corte1/Corte1/Clases/Operacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corte1.Clases;
+
+namespace Corte1.Clases
+{
+    class Operacion
+    {
+    }
+}
