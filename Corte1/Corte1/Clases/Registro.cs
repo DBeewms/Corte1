6dddc601b9d16897edf0f5c8f5a2177dbfc02361
DBeewms <dbwcontrol05@gmail.com>
2024-09-23 08:33:17 -0600

    Agregada clase Registro

---
+++ Corte1/Corte1/Clases/Registro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corte1.Clases;
+
+namespace Corte1.Clases
+{
+    internal class Registro
+    {
+        Persona persona = new Persona();
+    }
+}
